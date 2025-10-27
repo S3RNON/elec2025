046e4419061a7b34b1e5109c27510b4c33f5f4fa
S3RNON <lucaslnoord@gmail.com>
2025-10-27 19:28:36 +0100

    mobile support started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,13 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<link rel="icon" href="/favicon.svg" />
+			</head>
 			<body className={`${lexendDeca.variable} antialiased bg-gray-5`}>
 				{children}
 			</body>
